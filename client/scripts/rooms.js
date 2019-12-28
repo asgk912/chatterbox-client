d6275c64665046bf1d5459959510a3daf4d5cb30
asgk912 <asgk912@gmail.com>
2019-12-28 14:17:13 -0800

    corrected room add button error

--- client/scripts/rooms.js
+++ client/scripts/rooms.js
@@ -3,9 +3,11 @@ var Rooms = {
 
   add: function(roomname) {
     // check if room is added at the initialization of webpage or at the button click
-    roomname = roomname || FormView.$form.find('input#message').val();
+    if (typeof roomname === 'object' ) {
+      roomname = FormView.$form.find('input#message').val();
+    }
 
-    // add new room to the list and render it on the drop down menu 
+    // add new room to the list and render it on the drop down menu
     if (!Rooms.list[roomname]) {
       Rooms.list[roomname] = true;
       RoomsView.renderRoom(roomname);
